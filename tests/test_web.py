a97e41d77c4ba8913d61c4bda59b9a59e7e313a7
Milind Suresh Bableshwar <milindb@fb.com>
2020-05-15 22:09:12 -0700

    Added threading to roll function and abstracted the compute function

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,5 +1,6 @@
 import pytest
 import time
+import _thread
 
 from pages.read_stats import StatsReader
 from pages.load_game import DragonIssDockerLoader
@@ -32,39 +33,47 @@ def test_basic_simulator_game(browser):
   stats_reader = StatsReader(browser)
   controller = DragonController(browser)
   time.sleep(10)
+  _thread.start_new_thread(control_the_dragon,
+   (stats_reader.roll_error,
+    stats_reader.roll_error_rate,
+    controller.roll_right,
+    controller.roll_left))
+  time.sleep(300)
+
+def control_the_dragon(error_func, error_rate_func, increase_rate, decrease_rate):
   threshold = 0.3
   while True:
     time.sleep(0.2)
-    if (abs(stats_reader.roll_error()) < 2):
+    if (abs(error_func()) < 2):
       maxRate = 0.1
-    elif (abs(stats_reader.roll_error()) < 5):
+    elif (abs(error_func()) < 5):
       maxRate = 0.2
-    elif (abs(stats_reader.roll_error()) < 10):
+    elif (abs(error_func()) < 10):
       maxRate = 0.3
     else:
       maxRate = 0.4
 
-    if (abs(stats_reader.roll_error()) <= threshold):
-      if (abs(stats_reader.roll_error_rate()) > 0.0):
-        if (stats_reader.roll_error_rate() < 0):
-          controller.roll_right()
-        elif (stats_reader.roll_error_rate() > 0):
-          controller.roll_left()
+    if (abs(error_func()) <= threshold):
+      if (abs(error_rate_func()) > 0.0):
+        if (error_rate_func() < 0):
+          increase_rate()
+        elif (error_rate_func() > 0):
+          decrease_rate()
       else:
-        print ('Success!!!')
+        print ('Equilibrium!!!')
         time.sleep(3)
 
-    if (stats_reader.roll_error() > threshold and stats_reader.roll_error_rate() < maxRate):
-      print ("Rolling right ...")
-      controller.roll_right()
-    elif (stats_reader.roll_error() > threshold and stats_reader.roll_error_rate() > maxRate):
-      print ("decreasing right rate ...")
-      controller.roll_left()
-    elif (stats_reader.roll_error() < -threshold and stats_reader.roll_error_rate() > -maxRate):
-      print ("Rolling left ...")
-      controller.roll_left()
-    elif (stats_reader.roll_error() < -threshold and stats_reader.roll_error_rate() < -maxRate):
-      print ("decreasing left rate ...")
-      controller.roll_right()
+    if (error_func() > threshold and error_rate_func() < maxRate):
+      # Increasing rate within max rate limit because of error
+      increase_rate()
+    elif (error_func() > threshold and error_rate_func() > maxRate):
+      # Decreasing increased rate because of max rate limit
+      decrease_rate()
+    elif (error_func() < -threshold and error_rate_func() > -maxRate):
+      # Decreasing rate within max rate limit because of error
+      decrease_rate()
+    elif (error_func() < -threshold and error_rate_func() < -maxRate):
+      # Increasing decreased rate because of max rate limit
+      increase_rate()
 
-    print("Roll: " + str(stats_reader.roll_error()) + " with rate " + str(stats_reader.roll_error_rate()))
+    print("Error: " + str(error_func()) + " Error rate " + str(error_rate_func()))

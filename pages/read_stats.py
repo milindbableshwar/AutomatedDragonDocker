c37d043381fdaf0f35a46f2e1ae419aaa2c8b759
Milind Suresh Bableshwar <milindb@fb.com>
2020-05-15 19:38:40 -0700

    Added automatic control for roll correction

--- pages/read_stats.py
+++ pages/read_stats.py
@@ -1,6 +1,39 @@
+import re
+
 from selenium.webdriver.common.by import By
 
 
 class StatsReader:
+
+  ROLL_ERROR_ID = (By.XPATH, f"//div[@id='hud']//div[@id='roll']//div[@class='error']")
+  ROLL_ERROR_RATE_ID = (By.XPATH, f"//div[@id='hud']//div[@id='roll']//div[@class='rate']")
+  PITCH_ERROR_ID = (By.XPATH, f"//div[@id='hud']//div[@id='pitch']//div[@class='error']")
+  PITCH_ERROR_RATE_ID = (By.XPATH, f"//div[@id='hud']//div[@id='pitch']//div[@class='rate']")
+  YAW_ERROR_ID = (By.XPATH, f"//div[@id='hud']//div[@id='yaw']//div[@class='error']")
+  YAW_ERROR_RATE_ID = (By.XPATH, f"//div[@id='hud']//div[@id='yaw']//div[@class='rate']")
+
+
   def __init__(self, browser):
     self.browser = browser
+
+  def parseFromDom(self, *id):
+    text = self.browser.find_element(*id).text
+    return float(re.findall(r"[-+]?\d*\.\d+|\d+", text)[0])
+
+  def roll_error(self):
+    return self.parseFromDom(*self.ROLL_ERROR_ID)
+
+  def pitch_error(self):
+    return self.parseFromDom(*self.PITCH_ERROR_ID)
+
+  def yaw_error(self):
+    return self.parseFromDom(*self.YAW_ERROR_ID)
+
+  def roll_error_rate(self):
+    return self.parseFromDom(*self.ROLL_ERROR_RATE_ID)
+
+  def pitch_error_rate(self):
+    return self.parseFromDom(*self.PITCH_ERROR_RATE_ID)
+
+  def yaw_error_rate(self):
+    return self.parseFromDom(*self.YAW_ERROR_RATE_ID)
--- tests/test_web.py
+++ tests/test_web.py
@@ -32,13 +32,39 @@ def test_basic_simulator_game(browser):
   stats_reader = StatsReader(browser)
   controller = DragonController(browser)
   time.sleep(10)
-  controller.roll_left()
-  controller.roll_left()
-  controller.roll_right()
-  controller.pitch_up()
-  controller.pitch_up()
-  controller.pitch_down()
-  controller.yaw_left()
-  controller.yaw_left()
-  controller.yaw_right()
-  time.sleep(10)
+  threshold = 0.3
+  while True:
+    time.sleep(0.2)
+    if (abs(stats_reader.roll_error()) < 2):
+      maxRate = 0.1
+    elif (abs(stats_reader.roll_error()) < 5):
+      maxRate = 0.2
+    elif (abs(stats_reader.roll_error()) < 10):
+      maxRate = 0.3
+    else:
+      maxRate = 0.4
+
+    if (abs(stats_reader.roll_error()) <= threshold):
+      if (abs(stats_reader.roll_error_rate()) > 0.0):
+        if (stats_reader.roll_error_rate() < 0):
+          controller.roll_right()
+        elif (stats_reader.roll_error_rate() > 0):
+          controller.roll_left()
+      else:
+        print ('Success!!!')
+        time.sleep(3)
+
+    if (stats_reader.roll_error() > threshold and stats_reader.roll_error_rate() < maxRate):
+      print ("Rolling right ...")
+      controller.roll_right()
+    elif (stats_reader.roll_error() > threshold and stats_reader.roll_error_rate() > maxRate):
+      print ("decreasing right rate ...")
+      controller.roll_left()
+    elif (stats_reader.roll_error() < -threshold and stats_reader.roll_error_rate() > -maxRate):
+      print ("Rolling left ...")
+      controller.roll_left()
+    elif (stats_reader.roll_error() < -threshold and stats_reader.roll_error_rate() < -maxRate):
+      print ("decreasing left rate ...")
+      controller.roll_right()
+
+    print("Roll: " + str(stats_reader.roll_error()) + " with rate " + str(stats_reader.roll_error_rate()))
